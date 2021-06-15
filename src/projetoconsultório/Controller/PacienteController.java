648865153b05bfff5611a89c4b2a7de5a1547e85
Italo Castro <italocesarcastroo@gmail.com>
2021-06-15 19:37:54 -0300

    COMITEI PARA TESTAR

--- src/projetoconsultório/Controller/PacienteController.java
+++ src/projetoconsultório/Controller/PacienteController.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package projetoconsultório.Model;
 
-/**
- *
- * @author marcelo
- */
-public class Paciente {
+package projetoconsultório.Controller;
+
+
+public class PacienteController {
     
 }
