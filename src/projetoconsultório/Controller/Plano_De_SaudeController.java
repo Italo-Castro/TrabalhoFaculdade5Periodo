31df2fd1b4e17288f41dfbca3bf63fd0cbd84816
Italo Castro <italocesarcastroo@gmail.com>
2021-06-15 19:52:40 -0300

    DiagramaClasse

--- src/projetoconsultório/Controller/Plano_De_SaudeController.java
+++ src/projetoconsultório/Controller/Plano_De_SaudeController.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package projetoconsultório.Model;
 
-/**
- *
- * @author marcelo
- */
-public class Plano_De_Saude {
+package projetoconsultório.Controller;
+
+
+public class Plano_De_SaudeController {
     
 }
